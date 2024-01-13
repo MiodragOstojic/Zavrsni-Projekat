13a2023c07e68d7184ef41b9418a9e6aa5201bb6
o.mijodrag@gmail.com <o.mijodrag@gmail.com>
2024-01-13 04:04:38 +0100

    dopuna prvog dela zavrsnog projekta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SocialSidebarComponent } from './social-sidebar/social-sidebar.component';
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'social-sidebar', component: SocialSidebarComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule] 
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,43 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SocialSidebarComponent } from './social-sidebar/social-sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { ShopComponent } from './shop/shop.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductComponent } from './product/product.component'; // Dodajte ovaj import ako koristite mat-input
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SocialSidebarComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    ShopComponent,
+    CategoryComponent,
+    ProductComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatIconModule,
+    MatInputModule,
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
